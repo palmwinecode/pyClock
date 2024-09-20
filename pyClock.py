96ec620c496684f0c3d2e07a7d70c786f8cec95c
Njom Jeremiah <njomjeremiah@gmail.com>
2024-09-20 18:36:11 +0100

    Added comments

--- pyClock.py
+++ pyClock.py
@@ -1,18 +1,30 @@
 from tkinter import *
 from tkinter.ttk import *
-
 from time import strftime
 
+# Initialize Tk widget
 root = Tk()
+
+# Add Tk widget title
 root.title("Clock")
 
+# Define time function  
 def time():
+    # Convert time tuple to string
     string = strftime("%I:%M:%S %p")
     label.config(text=string)
+
+    # Call function every second
     label.after(1000, time)
 
+# Add label
 label = Label(root, font=("ds-digital", 80), background="black", foreground="cyan")
+
+# Pack label
 label.pack(anchor="center")
+
+# Call time function
 time()
 
+# Call Tk mainloop
 root.mainloop()
